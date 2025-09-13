6429b01606ec4187cca8abfdb696cfe15f6e7372
IvanVijandi <ivijandi75@gmail.com>
2025-09-12 21:05:12 -0300

    Primer commit

---
+++ src/test/java/com/springbootticketingsystem/SpringbootTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootticketingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
